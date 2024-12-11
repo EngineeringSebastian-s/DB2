b2e19b6dd4ded560e3a2cfdbfedb87f67bebe3e8
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2024-12-10 20:51:01 -0500

    Project initialization

---
+++ Parcial#2/FashionLine-API/src/main/java/fashionline/com/api/FashionLineApiApplication.java
@@ -0,0 +1,13 @@
+package fashionline.com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionLineApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionLineApiApplication.class, args);
+    }
+
+}
---
+++ Parcial#2/FashionLine-API/src/test/java/fashionline/com/api/FashionLineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fashionline.com.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionLineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
